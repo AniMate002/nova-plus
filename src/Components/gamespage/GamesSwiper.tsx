e8767b245258f887dd78f30ac7d760305918c985
Alukard <kirill1974111@gmail.com>
2024-01-26 18:39:19 +0100

    Game page cards adaptivity

--- src/Components/gamespage/GamesSwiper.tsx
+++ src/Components/gamespage/GamesSwiper.tsx
@@ -17,9 +17,13 @@ const coverFLowSettings = {
 export const getRenderedTags = (tags: Array<string>) => {
     return tags.map((tag: string) => {
         return (
-            <p className='text-white bg-transparent border-[1px] border-white  rounded-full uppercase 
+            <p className='text-white bg-transparent border-[1px] border-white  rounded-full  
             text-[6px] px-1 py-0 lowercase
-            sm:text-sm sm:px-8 sm:py-4
+            min-[460px]:text-[8px] min-[460px]:px-2 min-[460px]:py-1
+            sm:text-[10px] sm:px-2 sm:py-1
+            lg:text-[12px] lg:px-4 lg:py-2 lg:uppercase
+            xl:text-sm xl:px-6 xl:py-2 xl:uppercase
+            2xl:text-sm 2xl:px-8 2xl:py-4 2xl:uppercase
             '>{tag}</p>
         )
     })
@@ -29,7 +33,10 @@ export const getRenderedPlatforms = (platforms: Array<string>) => {
     return platforms.map(platform => {
         return <p className='text-white  font-semibold
         text-[8px]
-        sm:text-xl
+        min-[460px]:text-[10px]
+        sm:text-sm
+        lg:text-lg
+        xl:text-xl
         '><i className={platform}></i></p>
     })
 }
@@ -52,26 +59,39 @@ const GamesSwiper = () => {
                     '>
                         <div className='rounded-full bg-white  text-violet-800 font-semibold w-fit
                         px-2 py-1 text-[8px]
-                        sm:px-8 sm:py-4 sm:text-base
+                        sm:px-2 sm:py-2 sm:text-[12px]
+                        lg:px-4 lg:py-2 lg:text-sm
+                        xl:px-8 xl:py-4 xl:text-base
                         '>featured</div>
                         <h2 className='text-center mt-[15%] text-white tracking-widest  font-bold
                         text-sm
-                        sm:text-7xl
+                        sm:text-xl
+                        min-[960px]:text-3xl
+                        lg:text-5xl
+                        xl:text-7xl
                         '>{game.title}</h2>
                         <div className='absolute 
                         bottom-2
-                        sm:bottom-10
+                        sm:bottom-4
+                        lg:bottom-6
+                        xl:bottom-10
+
                         '>
                             <div className=' flex gap-4 
 
                             mb-1 ml-1
-                            sm:mb-10 sm:ml-4
+                            sm:mb-2 sm:ml-2
+                            lg:mb-4 lg:ml-2
+                            xl:mb-6 xl:ml-4
+                            2xl:mb-10 2xl:ml-4
                             '>
                                 {getRenderedPlatforms(game.platforms)}
                             </div>
                             <div className='flex  flex-wrap
                             gap-0
-                            sm:gap-4
+                            sm:gap-1
+                            lg:gap-2
+                            xl:gap-4
                             '>
                                 {getRenderedTags(game.tags)}
                             </div>
