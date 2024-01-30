db52284932acff1d8dff746053a195cc3723ffca
Alukard <kirill1974111@gmail.com>
2024-01-30 23:21:23 +0100

    Not found page adaptivity

--- src/Components/Pages/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound.tsx
@@ -6,12 +6,32 @@ const PageNotFound:React.FC = () => {
     const navigate = useNavigate()
     const goHome = ():void => navigate('/')
     return (
-        <div className="min-h-[100vh] bg-gradient-to-bl from-violet-950 to-slate-950">
-            <div className="text-[350px] text-white opacity-[0.2] w-full text-center uppercase font-bold Haptik tracking-widest pt-10">
+        <div className="min-h-[100vh] bg-gradient-to-bl from-violet-950 to-slate-950 overflow-x-hidden">
+            <div className=" text-white opacity-[0.2] w-full text-center uppercase font-bold Haptik tracking-widest
+            text-7xl pt-40
+            min-[490px]:text-[120px]
+            sm:text-[150px]
+            md:text-[200px] md:pt-20
+            lg:text-[250px] lg:pt-20
+            xl:text-[340px] xl:pt-32
+            2xl:text-[350px] 2xl:pt-32
+            ">
                 error
             </div>
-            <div className="w-full text-white text-[250px] Haptik text-center mt-[-300px] font-bold">404</div>
-            <p className="text-center text-white Haptik text-[24px] tracking-wider makeNunito">The page you are looking for was either removed or doesn't exist.</p>
+            <div className="w-full text-white Haptik text-center font-bold
+            text-[100px]
+            min-[490px]:text-[130px]
+            sm:mt-[-100px]
+            lg:text-[170px] lg:mt-[-150px]
+            xl:text-[200px] xl:mt-[-190px]
+            2xl:text-[250px] 2xl:mt-[-200px]
+            ">404</div>
+            <p className="text-center text-white Haptik tracking-wider makeNunito
+            text-[10px]
+            min-[480px]:text-sm
+            sm:text-md
+            md:text-[24px]
+            ">The page you are looking for was either removed or doesn't exist.</p>
             <button onClick={goHome} className="mx-auto block mt-10 px-8 py-4 bg-violet-800 text-white rounded-lg hover:bg-violet-700 transition-all ">Go home</button>
         </div>
     );
